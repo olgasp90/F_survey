c073c84d7408a2b6b3baff762172460b9f12d6b9
Olga <olga_sprincean@yahoo.com>
2021-03-08 15:13:18 -0600

    add session

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import Survey, satisfaction_survey
+from surveys import satisfaction_survey
+
+response = 'responses'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some_secret_password'
@@ -8,26 +10,25 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
-
 @app.route('/')
 def home():
     """select survey"""
     TITLE = satisfaction_survey.title
     INSTRUCTIONS = satisfaction_survey.instructions
-    return render_template('survey.html', title=TITLE, instructions=INSTRUCTIONS)
+    return render_template('survey.html', title=TITLE, instructions=INSTRUCTIONS )
 
 
 @app.route("/begin", methods=["POST"])
 def start_survey():
     """redirect to the first question"""
+    session[response] = []
     return redirect("/questions/0")
 
 
 @app.route('/questions/<num>')
 def questions(num):
     num = int(float(num))
+    responses = session.get(response)
 
     if (responses is None):
         return redirect('/')
@@ -46,8 +47,10 @@ def questions(num):
 def survey_answers():
     answers = request.form['answer']
     """ add the answer to the responses list"""
+    responses = session[response]
     responses.append(answers)
-    print(responses)
+    session[response] = responses
+
     if(len(responses) == len(satisfaction_survey.questions)):
         return redirect('/complete')
     else:
